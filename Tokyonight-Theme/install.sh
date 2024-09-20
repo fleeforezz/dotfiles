5c149bd00e2d092241e08b9ecb36bb5916a65495
Trương Minh Nhật <fleeforezz@gmail.com>
2024-09-20 16:30:41 +0700

    Update

--- Tokyonight-Theme/install.sh
+++ Tokyonight-Theme/install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:455dcb72093b853ed8dac5329a7c90a8848d59df313bec18a91a677b7130c83d
-size 9994
+oid sha256:a9fb4b0fb9b9da2007b053e24446d0aa5d6929c9309ef3a38016c7dcd19d1df2
+size 10690
