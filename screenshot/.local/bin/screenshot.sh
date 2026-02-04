d5b3001783bb89770e904ea6bcf5fa77bf7a7910
Trương Minh Nhật <fleeforezz@gmail.com>
2026-02-04 10:25:17 +0700

    Update

--- screenshot/.local/bin/screenshot.sh
+++ screenshot/.local/bin/screenshot.sh
@@ -3,16 +3,10 @@
 DIR="$HOME/Pictures/Screenshots"
 mkdir -p "$DIR"
 
-FILE="$DIR/$(date '+%Y%m%d_%H%M%S').png"
-
+FILE="$DIR/$(date +%Y%m%d_%H%M%S).png"
 GEOM=$(slurp) || exit 1
 
-grim -g "$GEOM" - | satty \
-  --early-exit \
-  --action-on-enter save-to-file \
-  --right-click-copy \
-  --filename - \
-  --output-filename "$FILE"
-
-paplay /usr/share/sounds/freedesktop/stereo/screen-capture.oga
-notify-send "📸 Screenshot" "Saved & copied"
+grim -g "$GEOM" "$FILE" &&
+wl-copy < "$FILE" &&
+paplay /usr/share/sounds/freedesktop/stereo/screen-capture.oga &&
+notify-send "Screenshot" "Saved & copied to clipboard"
